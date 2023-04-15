cb24615c186faef3195e684f359e360b81993fed
Chris Tague <cstague6401@gmail.com>
2023-04-15 17:56:23 -0500

    adding amounts to ingrediets

--- src/components/search-ingredients.tsx
+++ src/components/search-ingredients.tsx
@@ -1,8 +1,9 @@
+import { RecipeIngredient } from "@/types";
 import { Input } from "./Input";
 import { useState } from "react";
 
 export type SearchIngredientsProps = {
-    onIngredientSelect: (ingredient: string) => void;
+    onIngredientSelect: (ingredient: RecipeIngredient) => void;
     buttonTitle: string;
 };
 
@@ -11,20 +12,26 @@ export const SearchIngredients = ({
     buttonTitle
 }: SearchIngredientsProps) => {
     const [input, setInput] = useState('');
+    const [amount, setAmount] = useState('');
     const handleButtonPress = async () => {
         const {status} = await fetch(`/api/ingredient/${input}`);
         
         if (status === 200) {
-            onIngredientSelect(input);
+            onIngredientSelect({
+              ingredient: input,
+              amount
+            });
             setInput('');
+            setAmount('');
         }
     }
 
     return (
       <div className="flex">
-        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        <Input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Ingredient"/>
+        <Input value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Amount"/>
         <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-1/5 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={handleButtonPress}
         >
             {buttonTitle}
