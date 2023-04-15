8b2aab178bcaccbb4bbf59da66ee5b1ed9f43ecb
Kyle Goben <kygoben@gmail.com>
2023-04-15 17:17:19 -0500

    valid ingredient

--- src/pages/api/ingredient/[ing].ts
+++ src/pages/api/ingredient/[ing].ts
@@ -1,13 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// pages/api/ingredient/[ing].ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from '../../../../supabaseClient';
 
-type Data = {
-  name: string
-}
+type Ingredient = {
+  name: string;
+  // Add other columns as needed
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function getIngredient(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const ing = req.query.ing;
+
+    const { data, error } = await supabase
+    .from('Ingredients')
+    .select(`
+        name`).eq('name', ing);
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    if (data && data.length > 0) {
+      return res.status(200).json(data[0]);
+    } else {
+      return res.status(404).json({ error: 'No ingredient found' });
+    }
+  } else {
+    res.setHeader('Allow', 'GET');
+    res.status(405).json({ error: `Method ${req.method} not allowed` });
+  }
 }
