9ac8ab8f036caa762aa6db9f98798ed934deb2db
Robby <riceslif@gmail.com>
2023-04-15 16:14:09 -0500

    example test page done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,9 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
   return (
