93632936d21541f42994d00197b5f7296fdd5b88
Kyle Goben <kygoben@gmail.com>
2023-04-16 04:54:12 -0500

    styling

--- src/pages/create-recipes.tsx
+++ src/pages/create-recipes.tsx
@@ -45,41 +45,49 @@ const CreateRecipes = () => {
     }
 
     return (
-        <div>
-            <Input
-                value={name}
-                onChange={e => setName(e.target.value)}
-                placeholder="Recipe Name"
-            />
-            <div>
-                {ingredients.map(({name, amount, tagName}) => {
-                    const displayName = name ? name : tagName
+        <div className="p-4 bg-gray-100 min-h-screen">
+            <div className="container mx-auto max-w-screen-lg">
+                <h1 className="text-4xl font-semibold text-center mb-6">Create Recipe</h1>
+                <div className="bg-white p-8 rounded-lg shadow-md">
+                    <Input
+                        className="mb-4"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        placeholder="Recipe Name"
+                    />
+                    <div className="mb-4">
+                        {ingredients.map(({name, amount, tagName}) => {
+                            const displayName = name ? name : tagName
 
-                    return (
-                        <div key={displayName}>{`${displayName} | ${amount}`}</div>
-                    );
-                })}
+                            return (
+                                <div key={displayName} className="border-b border-gray-200 py-2">
+                                    {`${displayName} | ${amount}`}
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <SearchIngredients
+                        onIngredientSelect={addIngredient}
+                        buttonTitle="Add"
+                    />
+                    <div className="flex items-center mb-4">
+                        <label htmlFor='is-public-check' className="mr-2">
+                            Is public
+                        </label>
+                        <Switch
+                            id='is-public-check'
+                            checked={isPublic}
+                            onChange={(_, checked) => setIsPublic(checked)}
+                        />
+                    </div>
+                    <button
+                        className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        onClick={createRecipe}
+                    >
+                        Submit
+                    </button>
+                </div>
             </div>
-            <SearchIngredients
-                onIngredientSelect={addIngredient}
-                buttonTitle="Add"
-            />
-            <label
-                htmlFor='is-public-check'
-            >
-                Is public
-            </label>
-            <Switch
-                id='is-public-check'
-                checked={isPublic}
-                onChange={(_, checked) => setIsPublic(checked)}
-            /><br/>
-            <button
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-1/5 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                onClick={createRecipe}
-            >
-                Submit
-            </button>
         </div>
     );
 }
